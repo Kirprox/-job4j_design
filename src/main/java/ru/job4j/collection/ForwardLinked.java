f7c63c831d74a4d70b065fe78500235cfbbee09f
kir <kirpro95@yandex.ru>
2024-08-04 18:53:07 +0300

    4. Используя контейнер на базе связанного списка создать контейнер Stack [#71474 #507679]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -27,6 +27,7 @@ public class ForwardLinked<T> implements Iterable<T> {
 
     public void addFirst(T value) {
         head = new Node<>(value, head);
+        size++;
     }
 
     public T get(int index) {
