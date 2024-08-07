cb3771f823acf3947fef96c04230e9c72da1a65d
kir <kirpro95@yandex.ru>
2024-08-07 15:13:17 +0300

    1. Реализовать коллекцию Set на массиве [#996]

---
+++ src/main/java/ru/job4j/set/SimpleArraySet.java
@@ -0,0 +1,58 @@
+package ru.job4j.set;
+
+import ru.job4j.collection.SimpleArrayList;
+
+import java.util.*;
+
+public class SimpleArraySet<T> implements SimpleSet<T> {
+
+    private SimpleArrayList<T> set = new SimpleArrayList<>(0);
+
+    @Override
+    public boolean add(T value) {
+        boolean result = true;
+        for (int i = 0; i < set.size(); i++) {
+            if (Objects.equals(set.get(i), value)) {
+                result = false;
+            }
+        }
+        if (result) {
+            set.add(value);
+        }
+        return result;
+    }
+
+    @Override
+    public boolean contains(T value) {
+        boolean result = false;
+        for (T e : set) {
+            if (Objects.equals(e, value)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return set.iterator();
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        SimpleArraySet<Integer> s = new SimpleArraySet<>();
+        s.add(1);
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        s.add(4);
+        s.add(5);
+        System.out.println(s.contains(1));
+        System.out.println(s.contains(2));
+        System.out.println(s.contains(3));
+        System.out.println(s.contains(4));
+        System.out.println(s.contains(5));
+    }
+}
