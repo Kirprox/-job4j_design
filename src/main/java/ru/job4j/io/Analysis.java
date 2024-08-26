ada82cadc961168edecaf6349c41ae3f83733163
kir <kirpro95@yandex.ru>
2024-08-26 14:01:09 +0300

    7. Анализ доступности сервера [#859 #510558]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -5,31 +5,21 @@ import java.util.*;
 
 public class Analysis {
     public void unavailable(String source, String target) {
-        List<String> input = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
-            input = reader.lines().toList();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        StringBuilder resultBuilder = new StringBuilder();
+        String line;
         boolean started = false;
-        for (String str : input) {
-            String[] pair = str.split(" ");
-            int condition = Integer.parseInt(pair[0]);
-            if ((condition == 500 || condition == 400) && !started) {
-                resultBuilder.append(pair[1] + ";");
-                started = true;
-            } else if ((condition == 300 || condition == 200) && started) {
-                resultBuilder.append(pair[1] + "\n");
-                started = false;
-            }
-        }
-        String[] result = resultBuilder.toString().split(System.lineSeparator());
-        try (PrintWriter output = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(target)))) {
-            for (String str : result) {
-                output.println(str);
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+             PrintWriter output = new PrintWriter(
+                     new BufferedOutputStream(
+                             new FileOutputStream(target)))) {
+            while ((line = reader.readLine()) != null) {
+                String[] pair = line.split(" ");
+                int condition = Integer.parseInt(pair[0]);
+                if ((condition == 500 || condition == 400) && !started) {
+                    output.print(pair[1] + ";");
+                    started = true;
+                } else if ((condition == 300 || condition == 200) && started) {
+                    output.println(pair[1]);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
