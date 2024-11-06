90994e7ddc2d489a8e24938e1fd245bf34847b6d
kir <kirpro95@yandex.ru>
2024-11-06 17:51:20 +0300

    14. Архивировать проект [#861]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,46 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    public void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path path : sources) {
+                zip.putNextEntry(new ZipEntry(path.toString()));
+                try (BufferedInputStream output =
+                             new BufferedInputStream(new FileInputStream(path.toString()))) {
+                    zip.write(output.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream output = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(output.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ArgsName argsName = ArgsName.of(args);
+        Path root = Path.of(argsName.get("d"));
+        String exclude = argsName.get("e");
+        File target = new File(argsName.get("o"));
+        List<Path> paths = Search.search(root, path ->
+                !path.toFile().getName().endsWith(exclude));
+        Zip zipProject = new Zip();
+        zipProject.packFiles(paths, target);
+    }
+}
